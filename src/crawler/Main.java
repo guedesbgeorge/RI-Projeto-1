0fd7fc58364ea465f193217191463c2db0499c10
guedesbgeorge <guedesbgeorge@gmail.com>
2017-04-04 14:11:30 -0300

    Renomeando pasta de dados do crawler

--- src/crawler/Main.java
+++ src/crawler/Main.java
@@ -2,7 +2,7 @@ package crawler;
 
 public class Main {
 	public static void main(String[] args) {
-		BreadthFirstCrawler crawler = new BreadthFirstCrawler("crawler/sites.txt");
+		BreadthFirstCrawler crawler = new BreadthFirstCrawler("crawler-data/sites.txt");
 		crawler.crawl();
 	}
 }
--- src/crawler/Spider.java
+++ src/crawler/Spider.java
@@ -15,7 +15,7 @@ import org.jsoup.select.Elements;
 
 public class Spider {
 	private ArrayList<String> linksFound = new ArrayList<String>();
-	private static String dir = "crawler/pages";
+	private static String dir = "crawler-data/pages";
 	private static File file = new File(dir);
 
 	public boolean visit(String url) {
