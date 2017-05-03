ff06fa6a60bfe1595d6e79c83b969bf2a3d8b8d4
imvm <imvm@cin.ufpe.br>
2017-05-03 01:13:48 -0300

    more classifiers evaluated

--- src/classifier/BatchClassifier.java
+++ src/classifier/BatchClassifier.java
@@ -7,7 +7,12 @@ package classifier;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.BayesNet;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.classifiers.functions.SMO;
+import weka.classifiers.functions.SimpleLogistic;
 import weka.classifiers.trees.J48;
+import weka.classifiers.trees.RandomForest;
 import weka.core.Instance;
 import weka.core.converters.TextDirectoryLoader;
 import weka.core.Instances;
@@ -43,11 +48,50 @@ public class BatchClassifier {
         }
     }
 
-    private void evaluateClassifier() throws Exception{
+    private void evaluateClassifier() throws Exception {
+        //J48 TREE
         Evaluation eval = new Evaluation(data);
         J48 tree = new J48();
         eval.crossValidateModel(tree, data, 10, new Random(1));
-        System.out.println(eval.toSummaryString("\nResults\n\n", false));
+        System.out.println(eval.toSummaryString("\nTree Results\n\n", false));
+
+        //BAYES NET
+        Evaluation eval2 = new Evaluation(data);
+        BayesNet bayes = new BayesNet();
+        eval2.crossValidateModel(bayes, data, 10, new Random(1));
+        System.out.println(eval2.toSummaryString("\nBayes Net Results\n\n", false));
+
+        //RANDOM FOREST
+        Evaluation eval3 = new Evaluation(data);
+        RandomForest rmf = new RandomForest();
+        eval3.crossValidateModel(rmf, data, 10, new Random(1));
+        System.out.println(eval3.toSummaryString("\nRandom Forest Results\n\n", false));
+
+        //NAIVE BAYES
+        Evaluation eval4 = new Evaluation(data);
+        NaiveBayes nb = new NaiveBayes();
+        eval4.crossValidateModel(nb, data, 10, new Random(1));
+        System.out.println(eval4.toSummaryString("\nNaive Bayes Results\n\n", false));
+
+        //LINEAR REGRESSION
+        Evaluation eval7 = new Evaluation(data);
+        SimpleLogistic regression = new SimpleLogistic();
+        eval7.crossValidateModel(regression, data, 10, new Random(1));
+        System.out.println(eval7.toSummaryString("\nLogistic Regression Results\n\n", false));
+
+        //SMO
+        Evaluation eval8 = new Evaluation(data);
+        SMO smoCls = new SMO();
+        eval8.crossValidateModel(smoCls, data, 10, new Random(1));
+        System.out.println(eval8.toSummaryString("\nSMO Results\n\n", false));
+
+        /*
+        //MULTILAYER PERCEPTRON
+        Evaluation eval6 = new Evaluation(data);
+        MultilayerPerceptron mp = new MultilayerPerceptron();
+        eval6.crossValidateModel(mp, data, 10, new Random(1));
+        System.out.println(eval6.toSummaryString("\nMultilayer Perceptron Results\n\n", false));
+        */
     }
 
     private void loadData() throws IOException {
