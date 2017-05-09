936bf721e100a57a331613580a6caaf8bdc0e29a
Danielle Sales <dso@cin.ufpe.br>
2017-05-09 13:37:25 -0300

    Mostra tempo de treinamento dos classificadores

--- src/classifier/BatchClassifier.java
+++ src/classifier/BatchClassifier.java
@@ -1,5 +1,6 @@
 package classifier;
 
+import weka.attributeSelection.InfoGainAttributeEval;
 /**
  * Created by Ian on 23/04/2017.
 
@@ -26,7 +27,9 @@ import weka.gui.treevisualizer.TreeVisualizer;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.Random;
 
 public class BatchClassifier {
@@ -53,58 +56,79 @@ public class BatchClassifier {
         //J48 TREE
         Evaluation eval = new Evaluation(data);
         J48 tree = new J48();
+        double before = System.currentTimeMillis();
         eval.crossValidateModel(tree, data, 10, new Random(1));
+        double after = System.currentTimeMillis();
         System.out.println(eval.toSummaryString("\nTree Results\n\n", false));
         System.out.println(eval.toClassDetailsString());
         System.out.println(eval.toMatrixString());
+        System.out.println("Training time " + (after - before) + " ms");
 
         //BAYES NET
         Evaluation eval2 = new Evaluation(data);
         BayesNet bayes = new BayesNet();
+        before = System.currentTimeMillis();
         eval2.crossValidateModel(bayes, data, 10, new Random(1));
+        after = System.currentTimeMillis();
         System.out.println(eval2.toSummaryString("\nBayes Net Results\n\n", false));
         System.out.println(eval2.toClassDetailsString());
         System.out.println(eval2.toMatrixString());
+        System.out.println("Training time " + (after - before) + " ms");
        
         //RANDOM FOREST
         Evaluation eval3 = new Evaluation(data);
         RandomForest rmf = new RandomForest();
+        before = System.currentTimeMillis();
         eval3.crossValidateModel(rmf, data, 10, new Random(1));
+        after = System.currentTimeMillis();
         System.out.println(eval3.toSummaryString("\nRandom Forest Results\n\n", false));
         System.out.println(eval3.toClassDetailsString());
         System.out.println(eval3.toMatrixString());
+        System.out.println("Training time " + (after - before) + " ms");
         
         //NAIVE BAYES
         Evaluation eval4 = new Evaluation(data);
         NaiveBayes nb = new NaiveBayes();
+        before = System.currentTimeMillis();
         eval4.crossValidateModel(nb, data, 10, new Random(1));
+        after = System.currentTimeMillis();
         System.out.println(eval4.toSummaryString("\nNaive Bayes Results\n\n", false));
         System.out.println(eval4.toClassDetailsString());
         System.out.println(eval4.toMatrixString());
+        System.out.println("Training time " + (after - before) + " ms");
 
         //LINEAR REGRESSION
         Evaluation eval7 = new Evaluation(data);
         SimpleLogistic regression = new SimpleLogistic();
+        before = System.currentTimeMillis();
         eval7.crossValidateModel(regression, data, 10, new Random(1));
+        after = System.currentTimeMillis();
         System.out.println(eval7.toSummaryString("\nLogistic Regression Results\n\n", false));
         System.out.println(eval7.toClassDetailsString());
         System.out.println(eval7.toMatrixString());
+        System.out.println("Training time " + (after - before) + " ms");
         
         //SMO
         Evaluation eval8 = new Evaluation(data);
         SMO smoCls = new SMO();
+        before = System.currentTimeMillis();
         eval8.crossValidateModel(smoCls, data, 10, new Random(1));
+        after = System.currentTimeMillis();
         System.out.println(eval8.toSummaryString("\nSMO Results\n\n", false));
         System.out.println(eval8.toClassDetailsString());
         System.out.println(eval8.toMatrixString());
+        System.out.println("Training time " + (after - before) + " ms");
         
         // SGD
         Evaluation eval9 = new Evaluation(data);
         SGD sgd = new SGD();
+        before = System.currentTimeMillis();
         eval9.crossValidateModel(sgd, data, 10, new Random(1));
+        after = System.currentTimeMillis();
         System.out.println(eval9.toSummaryString("\nSGD Results\n\n", false));
         System.out.println(eval9.toClassDetailsString());
         System.out.println(eval9.toMatrixString());
+        System.out.println("Training time " + (after - before) + " ms");
 
         /*
         //MULTILAYER PERCEPTRON
@@ -119,6 +143,7 @@ public class BatchClassifier {
         TextDirectoryLoader loader = new TextDirectoryLoader();
         loader.setDirectory(new File(data_location));
         data = loader.getDataSet();
+        data.setClassIndex(data.numAttributes() - 1);
         //System.out.println("\n\nImported data:\n\n" + data);
     }
 
