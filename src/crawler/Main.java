54a904ced293c9ff99c0fc26ccebfa91b0bd8776
guedesbgeorge <guedesbgeorge@gmail.com>
2017-05-18 23:47:26 -0300

    Ultimas alterações feitas para testes

--- src/crawler/Main.java
+++ src/crawler/Main.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class Main {
 	public static void main(String[] args) {
 		ArrayList<String> urls = CrawlerUtil.getLinesFromFile("crawler-data/sites.txt");
-		for(int i = 3; i < 6; i++) {
+		for(int i = 6; i < 7; i++) {
 			String url = urls.get(i);
 			final int siteIndex = i + 1;
 			new Thread(() -> {
-				//final String heuristicWordsFileName = "crawler-data/heuristic_words.txt";
-				BreadthFirstCrawler crawler = new BreadthFirstCrawler(url, siteIndex);
+				final String heuristicWordsFileName = "crawler-data/heuristic_words.txt";
+				HeuristicCrawler crawler = new HeuristicCrawler(url, heuristicWordsFileName, siteIndex);
 				crawler.crawl();
 			}).start();
 		}			
